78b377b61447143ca50cbc4311ff4f98fd863f77
wozaki <ozaki.shun5@gmail.com>
2016-01-02 12:57:41 +0900

    Update favorited tweet state

--- redux/src/main/renderer/reducers/timeline.js
+++ redux/src/main/renderer/reducers/timeline.js
@@ -1,4 +1,4 @@
-import {RECEIVED_HOME_TIMELINE} from '../constants/ActionTypes'
+import {RECEIVED_HOME_TIMELINE, CREATED_FAVORITE, DESTROYED_FAVORITE} from '../constants/ActionTypes'
 
 const initialState = {tweets: []};
 
@@ -8,6 +8,23 @@ export default function timeline(state = initialState, action) {
             return {
                 tweets: action.tweets.concat(state.tweets)
             };
+
+        case CREATED_FAVORITE:
+        case DESTROYED_FAVORITE:
+            const currentTweets = state.tweets;
+            const updatedTweet = action.tweet;
+            const replacedTweets = currentTweets.map(tweet => {
+                if (tweet.id_str == updatedTweet.id_str) {
+                    return updatedTweet;
+                } else {
+                    return tweet;
+                }
+            });
+
+            return {
+                tweets: replacedTweets
+            };
+
         default:
             return state;
     }
