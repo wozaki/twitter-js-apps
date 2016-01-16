05195dbcaba88fe9e41d5e3c6b99bca3671daab1
wozaki <ozaki.shun5@gmail.com>
2016-01-17 01:22:21 +0900

    Test ToggleFavoriteButton

---
+++ redux/src/test/mock-dom.js
@@ -0,0 +1,17 @@
+import jsdom from 'jsdom'
+var doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+var win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+propagateToGlobal(win);
+
+function propagateToGlobal(window) {
+    for (let key in window) {
+        if (!window.hasOwnProperty(key)) continue;
+        if (key in global) continue;
+
+        global[key] = window[key];
+    }
+}
