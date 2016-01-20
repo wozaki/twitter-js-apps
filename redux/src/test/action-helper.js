4f28c993667fd5d90877106ebd083bbe0f65206e
wozaki <ozaki.shun5@gmail.com>
2016-01-21 00:46:04 +0900

    [refactor] Extract mockStore

---
+++ redux/src/test/action-helper.js
@@ -0,0 +1,38 @@
+import expect from 'expect'
+import { applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk];
+export default function mockStore(getState, expectedActions, onLastAction) {
+  if (!Array.isArray(expectedActions)) {
+    throw new Error('expectedActions should be an array of expected actions.')
+  }
+  if (typeof onLastAction !== 'undefined' && typeof onLastAction !== 'function') {
+    throw new Error('onLastAction should either be undefined or function.')
+  }
+
+  function mockStoreWithoutMiddleware() {
+    return {
+      getState() {
+        return typeof getState === 'function' ?
+          getState() :
+          getState
+      },
+
+      dispatch(action) {
+        const expectedAction = expectedActions.shift();
+        expect(action).toEqual(expectedAction);
+        if (onLastAction && !expectedActions.length) {
+          onLastAction()
+        }
+        return action
+      }
+    }
+  }
+
+  const mockStoreWithMiddleware = applyMiddleware(
+    ...middlewares
+  )(mockStoreWithoutMiddleware);
+
+  return mockStoreWithMiddleware()
+}
--- redux/src/test/actions/favorite.spec.js
+++ redux/src/test/actions/favorite.spec.js
@@ -1,46 +1,10 @@
 import expect from 'expect'
 import rewire from 'rewire'
 import sinon from 'sinon'
-import { applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
 import * as types from '../../main/renderer/constants/ActionTypes'
+import mockStore from '../action-helper'
 
 let favoriteActions = rewire('../../main/renderer/actions/favorite');
-
-const middlewares = [thunk];
-function mockStore(getState, expectedActions, onLastAction) {
-    if (!Array.isArray(expectedActions)) {
-        throw new Error('expectedActions should be an array of expected actions.')
-    }
-    if (typeof onLastAction !== 'undefined' && typeof onLastAction !== 'function') {
-        throw new Error('onLastAction should either be undefined or function.')
-    }
-
-    function mockStoreWithoutMiddleware() {
-        return {
-            getState() {
-                return typeof getState === 'function' ?
-                    getState() :
-                    getState
-            },
-
-            dispatch(action) {
-                const expectedAction = expectedActions.shift();
-                expect(action).toEqual(expectedAction);
-                if (onLastAction && !expectedActions.length) {
-                    onLastAction()
-                }
-                return action
-            }
-        }
-    }
-
-    const mockStoreWithMiddleware = applyMiddleware(
-        ...middlewares
-    )(mockStoreWithoutMiddleware);
-
-    return mockStoreWithMiddleware()
-}
 
 describe('favorite actions', () => {
     function stubTwitterClient(stub) {
