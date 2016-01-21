5393738f02115dda16d47fa07876a4d1e9c3ce70
wozaki <ozaki.shun5@gmail.com>
2016-01-22 00:24:22 +0900

    Narrow BrowserWindow width

--- redux/src/main/browser/main-window.js
+++ redux/src/main/browser/main-window.js
@@ -2,7 +2,7 @@ import BrowserWindow from 'browser-window'
 
 export default class MainWindow {
     constructor() {
-        this.window = new BrowserWindow({width: 1200, height: 800});
+        this.window = new BrowserWindow({width: 500, height: 800});
         this.window.loadURL(`file://${__dirname}/../renderer/index.html`);
         this.window.on('closed', () => {
             this.window = null;
