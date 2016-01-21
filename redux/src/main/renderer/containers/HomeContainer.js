516b2191d355053d94bbd0d9cc4b74acbf2ed259
wozaki <ozaki.shun5@gmail.com>
2016-01-22 00:16:03 +0900

    [lint] HomeContainer

--- redux/src/main/renderer/containers/HomeContainer.js
+++ redux/src/main/renderer/containers/HomeContainer.js
@@ -1,36 +1,33 @@
-import _ from 'lodash';
-import React, { Component, PropTypes } from 'react'
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import Header from '../components/Header'
-import TweetsContainer from '../containers/TweetsContainer'
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import TweetsContainer from '../containers/TweetsContainer';
 
 export default class HomeContainer extends Component {
 
-    get title() {
-        return 'Home';
-    }
+  get title() {
+    return 'Home';
+  }
 
-    render() {
-        return (
-            <main className="main">
-                <Header title={this.title}/>
-                <TweetsContainer />
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main className="main">
+        <Header title={this.title}/>
+        <TweetsContainer />
+      </main>
+    );
+  }
 }
 
 HomeContainer.propTypes = {
-    account: PropTypes.object.isRequired
+  account: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
-    const {account} = state;
-    return {
-        account: account
-    }
+  const { account } = state;
+  return {
+    account: account
+  };
 }
 
-export default connect(mapStateToProps)(HomeContainer)
+export default connect(mapStateToProps)(HomeContainer);
