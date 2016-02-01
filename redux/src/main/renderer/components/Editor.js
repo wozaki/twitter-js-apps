2e44fd50d0e5d3a1628067a00793e31b5c4f6ef0
wozaki <ozaki.shun5@gmail.com>
2016-02-01 22:56:21 +0900

    Lint: Editor

--- redux/src/main/renderer/components/Editor.js
+++ redux/src/main/renderer/components/Editor.js
@@ -1,74 +1,78 @@
-import React, {Component} from 'react'
-import keyStringDetector from '../registries/keyStringDetector'
-import Tweet from '../../domain/models/Tweet'
+import React, { Component, PropTypes } from 'react';
+import keyStringDetector from '../registries/keyStringDetector';
+import Tweet from '../../domain/models/Tweet';
 
 export default class Editor extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = this.initialState();
+  constructor(props) {
+    super(props);
+    this.state = this.initialState();
+  }
+
+  onTextareaChanged(event) {
+    const text = event.target.value;
+    this.setState({ text: text }, () => {
+      const isExceededLimitCharLength = this.getRestTextLength() < 0;
+      this.setState({ isExceededLimitCharLength: isExceededLimitCharLength });
+    });
+  }
+
+  onTextareaKeyDown(event) {
+    if (keyStringDetector.detect(event) === 'Return') {
+      event.preventDefault();
+      this.onTweetSubmitted();
+    }
+  }
+
+  onTweetSubmitted() {
+    if (this.state.isExceededLimitCharLength) {
+      return;
     }
 
-    initialState() {
-        return {text: '', isExceededLimitCharLength: false};
-    }
+    const { onTweetSubmitted } = this.props;
 
-    getRestTextLength() {
-        return Tweet.LIMIT_CHARA_LENGTH - this.state.text.length;
-    }
+    onTweetSubmitted(this.state.text);
+    this.setState(this.initialState()); // TODO: tweetが成功したらテキストを初期化する
+  }
 
-    onTextareaChanged(event) {
-        const text = event.target.value;
-        this.setState({text: text}, () => {
-            const isExceededLimitCharLength = this.getRestTextLength() < 0;
-            this.setState({isExceededLimitCharLength: isExceededLimitCharLength});
-        });
-    }
+  getRestTextLength() {
+    return Tweet.LIMIT_CHARA_LENGTH - this.state.text.length;
+  }
 
-    onTextareaKeyDown(event) {
-        if (keyStringDetector.detect(event) === 'Return') {
-            event.preventDefault();
-            this.onTweetSubmitted();
-        }
-    }
+  initialState() {
+    return { text: '', isExceededLimitCharLength: false };
+  }
 
-    onTweetSubmitted() {
-        if (this.state.isExceededLimitCharLength) {
-            return;
-        }
+  get editorCounterClassName() {
+    return this.state.isExceededLimitCharLength
+      ? 'Editor-counter-exceeded'
+      : 'Editor-counter';
+  }
 
-        const {onTweetSubmitted} = this.props;
+  // TODO: 140字を超えたらviewを変更して伝える
+  render() {
+    return (
+      <div className="Editor">
+        <textarea
+          name="name"
+          rows="2"
+          cols="40"
+          className="Editor-textarea"
+          onChange={this.onTextareaChanged.bind(this)}
+          onKeyDown={this.onTextareaKeyDown.bind(this)}
+          placeholder="What's happening?"
+          value={this.state.text}>
+        </textarea>
 
-        onTweetSubmitted(this.state.text);
-        this.setState(this.initialState()); //TODO: tweetが成功したらテキストを初期化する
-    }
-
-    get editorCounterClassName() {
-        return this.state.isExceededLimitCharLength
-            ? "Editor-counter-exceeded"
-            : "Editor-counter";
-    }
-
-    //TODO: 140字を超えたらviewを変更して伝える
-    render() {
-        return (
-            <div className="Editor">
-                <textarea
-                    name="name"
-                    rows="2"
-                    cols="40"
-                    className="Editor-textarea"
-                    onChange={this.onTextareaChanged.bind(this)}
-                    onKeyDown={this.onTextareaKeyDown.bind(this)}
-                    placeholder="What's happening?"
-                    value={this.state.text}>
-                </textarea>
-
-                <div className={this.editorCounterClassName}>
-                    {this.getRestTextLength()}
-                </div>
-            </div>
-        );
-    }
+        <div className={this.editorCounterClassName}>
+          {this.getRestTextLength()}
+        </div>
+      </div>
+    );
+  }
 
 }
+
+Editor.propTypes = {
+  onTweetSubmitted: PropTypes.func.isRequired
+};
