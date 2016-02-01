6de7b4bef83c043918b0993da403ca8bc84575a3
wozaki <ozaki.shun5@gmail.com>
2016-02-01 22:59:34 +0900

    Lint: NewTweetContainer

--- redux/src/main/renderer/containers/NewTweetContainer.js
+++ redux/src/main/renderer/containers/NewTweetContainer.js
@@ -1,41 +1,41 @@
-import _ from 'lodash'
-import React, {Component, PropTypes} from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import _ from 'lodash';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import * as tweetActions from '../actions/tweet';
-import Editor from '../components/Editor'
+import Editor from '../components/Editor';
 
 export default class NewTweetContainer extends Component {
 
-    render() {
-        const {postTweet} = this.props.actions;
+  render() {
+    const { postTweet } = this.props.actions;
 
-        //TODO: render account image and place
-        //TODO: post image
+    // TODO: render account image and place
+    // TODO: post image
 
-        return (
-            <Editor key="editor" onTweetSubmitted={postTweet}/>
-        )
-    }
+    return (
+      <Editor key="editor" onTweetSubmitted={postTweet}/>
+    );
+  }
 }
 
 NewTweetContainer.propTypes = {
-    actions: PropTypes.object.isRequired,
-    account: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  account: PropTypes.object.isRequired
 };
 
 function mapDispatchToProps(dispatch) {
-    const actions = _.assign({}, tweetActions);
-    return {
-        actions: bindActionCreators(actions, dispatch)
-    };
+  const actions = _.assign({}, tweetActions);
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
 }
 
 function mapStateToProps(state) {
-    const {account} = state;
-    return {
-        account: account
-    }
+  const { account } = state;
+  return {
+    account: account
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewTweetContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(NewTweetContainer);
