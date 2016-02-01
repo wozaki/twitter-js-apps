dbb0eb3fc8637e6c1115a935ea73407844d2258a
wozaki <ozaki.shun5@gmail.com>
2016-02-02 00:45:32 +0900

    Setup account in NewTweetContainer

--- redux/src/main/renderer/containers/NewTweetContainer.js
+++ redux/src/main/renderer/containers/NewTweetContainer.js
@@ -2,12 +2,19 @@ import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import * as appActions from '../actions/app';
 import * as tweetActions from '../actions/tweet';
 import Editor from '../components/Editor';
 
 export default class NewTweetContainer extends Component {
 
+  componentWillMount() {
+    const {setUp} = this.props.actions;
+    setUp();
+  }
+
   render() {
+    const { account } = this.props;
     const { postTweet } = this.props.actions;
 
     // TODO: render account image and place
@@ -25,7 +32,7 @@ NewTweetContainer.propTypes = {
 };
 
 function mapDispatchToProps(dispatch) {
-  const actions = _.assign({}, tweetActions);
+  const actions = _.assign({}, appActions, tweetActions);
   return {
     actions: bindActionCreators(actions, dispatch)
   };
