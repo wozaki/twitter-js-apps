dd30b63376a0e6a7f087770df0443d99f04b3bfa
wozaki <ozaki.shun5@gmail.com>
2016-02-08 23:29:10 +0900

    Implement my-timeline action and reducer

---
+++ redux/src/main/renderer/actions/my-timeline.js
@@ -0,0 +1,36 @@
+import * as types from '../constants/ActionTypes';
+import {timelineUsecase} from '../registries/usecases';
+
+export function fetchMyTimeline() {
+  return dispatch => {
+    timelineUsecase
+      .getMyTweets()
+      .then(({ tweets }) => {
+        dispatch(receivedMyTimeline(tweets));
+      });
+  };
+}
+
+export function fetchOldMyTimeline(tweetId) {
+  return dispatch => {
+    timelineUsecase
+      .getMyTweetsOlderThan(tweetId)
+      .then(({ tweets }) => {
+        dispatch(receivedOldMyTimeline(tweets));
+      });
+  };
+}
+
+function receivedMyTimeline(tweets) {
+  return {
+    type: types.RECEIVED_MY_TIMELINE,
+    tweets
+  };
+}
+
+function receivedOldMyTimeline(tweets) {
+  return {
+    type: types.RECEIVED_OLD_MY_TIMELINE,
+    tweets
+  };
+}
---
+++ redux/src/main/renderer/reducers/my-timeline.js
@@ -0,0 +1,39 @@
+import { RECEIVED_MY_TIMELINE, RECEIVED_OLD_MY_TIMELINE, CREATED_FAVORITE, DESTROYED_FAVORITE} from '../constants/ActionTypes'
+
+const initialState = {tweets: [], isOld: false};
+
+export default function timeline(state = initialState, action) {
+    switch (action.type) {
+        case RECEIVED_MY_TIMELINE:
+            return {
+                tweets: action.tweets.concat(state.tweets),
+                isOld: false
+            };
+
+        case RECEIVED_OLD_MY_TIMELINE:
+            return {
+                tweets: state.tweets.concat(action.tweets),
+                isOld: true
+            };
+
+        case CREATED_FAVORITE:
+        case DESTROYED_FAVORITE:
+            const currentTweets = state.tweets;
+            const updatedTweet = action.tweet;
+            const replacedTweets = currentTweets.map(tweet => {
+                if (tweet.id_str == updatedTweet.id_str) {
+                    return updatedTweet;
+                } else {
+                    return tweet;
+                }
+            });
+
+            return {
+                tweets: replacedTweets,
+                isOld: false
+            };
+
+        default:
+            return state;
+    }
+}
