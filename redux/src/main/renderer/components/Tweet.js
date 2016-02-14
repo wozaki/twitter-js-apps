3c41ea990af3383db48ee6118fbfa340acc3ad0c
wozaki <ozaki.shun5@gmail.com>
2016-02-14 22:51:58 +0900

    [lint] Tweet

--- redux/src/main/renderer/components/Tweet.js
+++ redux/src/main/renderer/components/Tweet.js
@@ -3,42 +3,42 @@ import TweetBody from './TweetBody'
 import Time from './Time'
 
 export default class Tweet extends Component {
-    get url() {
-        return `https://twitter.com/${this.props.tweet.user.screen_name}/status/${this.props.tweet.id_str}`;
-    }
+  get url() {
+    return `https://twitter.com/${this.props.tweet.user.screen_name}/status/${this.props.tweet.id_str}`;
+  }
 
-    onAnchorClicked(event) {
-    }
+  onAnchorClicked(event) {
+  }
 
-    render() {
-        const {tweet, favoriteButton} = this.props;
+  render() {
+    const {tweet, favoriteButton} = this.props;
 
-        return (
-            <li className="Tweet">
-                <div className="Tweet-sub">
-                    <img className="Tweet-avatar" src={tweet.user.profile_image_url} height="48" width="48"/>
-                </div>
-                <div className="Tweet-main">
-                    <div className="Tweet-header">
-                        <div className="Tweet-names">
-                            <span className="Tweet-display-name">{tweet.user.name}</span>
-                            <span className="Tweet-screen-name">@{tweet.user.screen_name}</span>
-                        </div>
-                        <a className="Tweet-datetime-anchor" href={this.url} onClick={this.onAnchorClicked.bind(this)}
-                           tabIndex="-1">
-                            <Time className="Tweet-datetime" time={tweet.created_at}/>
-                        </a>
-                    </div>
-                    <div className="Tweet-body-container">
-                        <TweetBody tweet={tweet}/>
+    return (
+      <li className="Tweet">
+        <div className="Tweet-sub">
+          <img className="Tweet-avatar" src={tweet.user.profile_image_url} height="48" width="48"/>
+        </div>
+        <div className="Tweet-main">
+          <div className="Tweet-header">
+            <div className="Tweet-names">
+              <span className="Tweet-display-name">{tweet.user.name}</span>
+              <span className="Tweet-screen-name">@{tweet.user.screen_name}</span>
+            </div>
+            <a className="Tweet-datetime-anchor" href={this.url} onClick={this.onAnchorClicked.bind(this)}
+               tabIndex="-1">
+              <Time className="Tweet-datetime" time={tweet.created_at}/>
+            </a>
+          </div>
+          <div className="Tweet-body-container">
+            <TweetBody tweet={tweet}/>
 
-                        <div className="Tweet-buttons">
-                            {favoriteButton}
-                            <i className="fa fa-reply Tweet-button-reply"/>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        );
-    }
+            <div className="Tweet-buttons">
+              {favoriteButton}
+              <i className="fa fa-reply Tweet-button-reply"/>
+            </div>
+          </div>
+        </div>
+      </li>
+    );
+  }
 }
