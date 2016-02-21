2140d91010563d9ba386f41279430e9399fe9c85
wozaki <ozaki.shun5@gmail.com>
2016-02-22 00:44:07 +0900

    Implement getFollowingOlderThan to FollowingUsecase

--- redux/src/main/domain/usecases/FollowingUsecase.js
+++ redux/src/main/domain/usecases/FollowingUsecase.js
@@ -16,4 +16,14 @@ export default class FollowingUsecase {
     return this.twitterClient.friendsList({ userId: myId, count: count });
   }
 
+  /**
+   * @param {string} myId
+   * @param {string} nextCursor
+   * @param {number} count
+   * @returns {Promise.<Object>}
+   */
+  getFollowingOlderThan(myId, nextCursor, count) {
+    return this.twitterClient.friendsList({ userId: myId, cursor: nextCursor, count: count });
+  }
+
 }
