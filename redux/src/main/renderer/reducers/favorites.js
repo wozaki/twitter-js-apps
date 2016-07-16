b52d59ec125fcf8c563fbca6bdf1e3fa34a01632
wozaki <ozaki.shun5@gmail.com>
2016-07-16 14:03:07 +0900

    #26 Remove unfavorite tweet from FavoritesView

--- redux/src/main/renderer/reducers/favorites.js
+++ redux/src/main/renderer/reducers/favorites.js
@@ -1,4 +1,4 @@
-import { RECEIVED_MY_FAVORITES, RECEIVED_MY_OLD_FAVORITES } from '../constants/ActionTypes';
+import { RECEIVED_MY_FAVORITES, RECEIVED_MY_OLD_FAVORITES, DESTROYED_FAVORITE } from '../constants/ActionTypes';
 
 const initialState = { tweets: [], isOld: false };
 
@@ -14,6 +14,12 @@ export default function favorites(state = initialState, action) {
       return {
         tweets: state.tweets.concat(action.tweets),
         isOld: true
+      };
+
+    case DESTROYED_FAVORITE:
+      return {
+        tweets: state.tweets.filter(t => t.id_str !== action.tweet.id_str),
+        isOld: false
       };
 
     default:
