a3ea95877c2096f9bd7c0e003a0a3e0bf2959034
wozaki <ozaki.shun5@gmail.com>
2016-09-11 14:04:52 +0900

    [Refactor] Implement BaseWindow

---
+++ redux/src/main/browser/base-window.js
@@ -0,0 +1,34 @@
+import { BrowserWindow } from 'electron'
+
+class BaseWindow {
+  constructor(url, options) {
+    this.onClosedHandler = null;
+    this.browserWindow = new BrowserWindow(options);
+    this.browserWindow.loadURL(url);
+    this.browserWindow.on('closed', () => {
+      if (this.onClosedHandler != null) {
+        this.onClosedHandler();
+      }
+      this.browserWindow = null;
+    });
+  }
+
+  show() {
+    this.browserWindow.show();
+  }
+
+  hide() {
+    this.browserWindow.hide();
+  }
+
+  setClosedHandler(handler) {
+    this.onClosedHandler = handler;
+  }
+
+  send(...args) {
+    this.browserWindow.webContents.send(...args);
+  }
+
+}
+
+export default BaseWindow
