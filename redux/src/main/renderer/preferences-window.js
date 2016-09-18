2b47f91503de19d1594a293eeb9f329254750af5
wozaki <ozaki.shun5@gmail.com>
2016-09-19 01:32:12 +0900

    Use PreferencesContainer

--- redux/src/main/renderer/preferences-window.js
+++ redux/src/main/renderer/preferences-window.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
+import PreferencesContainer from './containers/prefrence/PreferencesContainer'
 
 const store = configureStore();
 
 render(
     <Provider store={store}>
+        <PreferencesContainer />
     </Provider>,
     document.getElementById('root')
 );
