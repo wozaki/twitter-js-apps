2044209b8d3a0adef68bcdb7d52ed72bb9834cad
wozaki <ozaki.shun5@gmail.com>
2016-09-19 13:22:18 +0900

    micro fix

--- redux/src/main/renderer/store/configureStore.js
+++ redux/src/main/renderer/store/configureStore.js
@@ -3,12 +3,12 @@ import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers'
 import persistState from 'redux-localstorage'
 
-const KEY_STORE_LOCAL_STORAGE = "store_local_storage";
+const KEY_REDUX_LOCAL_STORAGE = "persistent_states";
 const persistentStates = ['account'];
 
 const enhancer = compose(
   applyMiddleware(thunkMiddleware),
-  persistState(persistentStates, { key: KEY_STORE_LOCAL_STORAGE }),
+  persistState(persistentStates, { key: KEY_REDUX_LOCAL_STORAGE }),
 );
 const store = createStore(rootReducer, enhancer);
 
