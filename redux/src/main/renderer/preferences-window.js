23e3c385412b3c9fd502965f2deac58d33f3ee0e
wozaki <ozaki.shun5@gmail.com>
2016-09-26 11:11:48 +0900

    Set window.registries in PreferencesWindow

--- redux/src/main/renderer/preferences-window.js
+++ redux/src/main/renderer/preferences-window.js
@@ -4,6 +4,9 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import PreferencesContainer from './containers/prefrence/PreferencesContainer'
 import store from './store/configureStore'
+import * as registries from './registries/electron'
+
+window.registries = registries;
 
 render(
     <Provider store={store}>
