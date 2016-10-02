2b40e80d58979f792a200ed0ab57382e453dc601
wozaki <ozaki.shun5@gmail.com>
2016-10-03 00:41:33 +0900

    [Bugfix] infinite calling onLoading in InfiniteScroll

--- redux/src/main/renderer/components/InfiniteScroll.js
+++ redux/src/main/renderer/components/InfiniteScroll.js
@@ -4,15 +4,38 @@ import _ from 'lodash';
 
 export default class InfiniteScroll extends Component {
 
+  componentWillReceiveProps(nextProps) {
+    const currentChildren = this.props.children;
+    const nextChildren    = nextProps.children;
+
+    if (currentChildren != nextChildren) {
+      this.state = { isLoading: false };
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: false, lastScrollTop: 0 };
+  }
+
   onScrolled() {
     const { children, onLoad, thresholdInPx }       = this.props;
+    const { isLoading }                             = this.state;
     const { scrollTop, scrollHeight, offsetHeight } = ReactDOM.findDOMNode(this);
     const restOfBottom                              = (scrollHeight - offsetHeight) - scrollTop;
 
-    if (restOfBottom < thresholdInPx) {
+    if (isLoading == false && restOfBottom < thresholdInPx && this._isDownScroll(scrollTop)) {
+      this.setState({ isLoading: true });
       onLoad(_.last(children));
     }
+
+    this.setState({ lastScrollTop: scrollTop });
   }
+
+  _isDownScroll = (scrollTop) => {
+    const { lastScrollTop } = this.state;
+    return scrollTop > lastScrollTop;
+  };
 
   render() {
     const { children, className, interval } = this.props;
