8dcad0a89cd46e34d3d86e2f5dc2081aaf7c9a33
wozaki <ozaki.shun5@gmail.com>
2016-10-10 02:33:24 +0900

    Implement WindowManager to manage all windows more easily

---
+++ redux/src/main/browser/window-manager.js
@@ -0,0 +1,50 @@
+import _ from "lodash"
+
+class WindowManager {
+  constructor() {
+    this._windows = {};
+  }
+
+  /**
+   * @param window {BaseWindow}
+   */
+  register(...window) {
+    _.each(window, (w) => {
+      this._windows[w.key] = w;
+    });
+  }
+
+  /**
+   * @param key {string}
+   * @return {BaseWindow}
+   */
+  find(key) {
+    return this._windows[key];
+  }
+
+  /**
+   * @param key {string}
+   */
+  ensure(key) {
+    const window = this.find(key);
+    window.show();
+    window.focus();
+  }
+
+  toggleDevTools() {
+    this._toAllWindows((window) => window.toggleDevTools());
+  }
+
+  reload() {
+    this._toAllWindows((window) => window.reload());
+  }
+
+  _toAllWindows(fn) {
+    _.each(this._windows, (window) => {
+      fn(window);
+    });
+  }
+
+}
+
+export default WindowManager
