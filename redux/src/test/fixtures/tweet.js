1510e8025893aaa64a71b9152d3a05ab6ae1b3ba
wozaki <ozaki.shun5@gmail.com>
2016-11-20 14:28:56 +0900

    Add tweet fixture

---
+++ redux/src/test/fixtures/tweet.js
@@ -0,0 +1,280 @@
+export const tweet1 = {
+  "coordinates": null,
+  "created_at": "Mon Sep 10 14:04:58 +0000 2012",
+  "truncated": false,
+  "favorited": false,
+  "id_str": "245160944223793152",
+  "in_reply_to_user_id_str": null,
+  "entities": {
+    "urls": [
+      {
+        "expanded_url": "http://bit.ly/MuCCDo",
+        "url": "http://t.co/W2tON3OK",
+        "indices": [
+          41,
+          61
+        ],
+        "display_url": "bit.ly/MuCCDo"
+      }
+    ],
+    "hashtags": [
+      {
+        "text": "TorontoFC",
+        "indices": [
+          87,
+          97
+        ]
+      },
+      {
+        "text": "MLS",
+        "indices": [
+          98,
+          102
+        ]
+      }
+    ],
+    "user_mentions": [
+      {
+        "name": "Team Up Foundation",
+        "id_str": "210844741",
+        "id": 210844741,
+        "indices": [
+          76,
+          86
+        ],
+        "screen_name": "TeamUpFdn"
+      }
+    ]
+  },
+  "text": "Create your own TFC ESQ by Movado Watch: http://t.co/W2tON3OK in support of @TeamUpFdn #TorontoFC #MLS",
+  "contributors": null,
+  "id": 245160944223793152,
+  "retweet_count": 0,
+  "in_reply_to_status_id_str": null,
+  "geo": null,
+  "retweeted": false,
+  "possibly_sensitive": false,
+  "in_reply_to_user_id": null,
+  "in_reply_to_screen_name": null,
+  "source": "TweetDeck",
+  "user": {
+    "profile_sidebar_fill_color": "EB1D31",
+    "profile_background_tile": false,
+    "profile_sidebar_border_color": "FFFFFF",
+    "name": "Toronto FC",
+    "profile_image_url": "http://a0.twimg.com/profile_images/1827235104/TorontoFC1_normal.jpg",
+    "created_at": "Fri Sep 11 15:42:26 +0000 2009",
+    "location": "Toronto, ON",
+    "follow_request_sent": false,
+    "is_translator": false,
+    "id_str": "73412535",
+    "profile_link_color": "000000",
+    "entities": {
+      "url": {
+        "urls": [
+          {
+            "expanded_url": null,
+            "url": "http://www.torontofc.ca",
+            "indices": [
+              0,
+              23
+            ],
+            "display_url": null
+          }
+        ]
+      },
+      "description": {
+        "urls": []
+      }
+    },
+    "favourites_count": 2,
+    "url": "http://www.torontofc.ca",
+    "default_profile": false,
+    "contributors_enabled": false,
+    "profile_image_url_https": "https://si0.twimg.com/profile_images/1827235104/TorontoFC1_normal.jpg",
+    "utc_offset": -18000,
+    "id": 73412535,
+    "listed_count": 1078,
+    "profile_use_background_image": true,
+    "followers_count": 28281,
+    "protected": false,
+    "profile_text_color": "000000",
+    "lang": "en",
+    "profile_background_color": "BC1228",
+    "verified": true,
+    "time_zone": "Eastern Time (US & Canada)",
+    "profile_background_image_url_https": "https://si0.twimg.com/profile_background_images/603424485/k9py0fm18qrjr8l22qlp.jpeg",
+    "notifications": false,
+    "description": "Official Toronto FC Twitter by @AsifinToronto & @JonSinden. Follow us for the latest club news, links, pics & videos. Join us during matches for #TFClive",
+    "geo_enabled": false,
+    "default_profile_image": false,
+    "friends_count": 13947,
+    "profile_background_image_url": "http://a0.twimg.com/profile_background_images/603424485/k9py0fm18qrjr8l22qlp.jpeg",
+    "statuses_count": 10774,
+    "screen_name": "torontofc",
+    "following": true,
+    "show_all_inline_media": false
+  },
+  "place": null,
+  "in_reply_to_status_id": null
+};
+
+export const tweet2 = {
+  "coordinates": {
+    "coordinates": [
+      -122.25831,
+      37.871609
+    ],
+    "type": "Point"
+  },
+  "truncated": false,
+  "created_at": "Tue Aug 28 21:08:15 +0000 2012",
+  "favorited": false,
+  "id_str": "240556426106372096",
+  "in_reply_to_user_id_str": null,
+  "entities": {
+    "urls": [
+      {
+        "expanded_url": "http://blogs.ischool.berkeley.edu/i290-abdt-s12/",
+        "url": "http://t.co/bfj7zkDJ",
+        "indices": [
+          79,
+          99
+        ],
+        "display_url": "blogs.ischool.berkeley.edu/i290-abdt-s12/"
+      }
+    ],
+    "hashtags": [],
+    "user_mentions": [
+      {
+        "name": "Cal",
+        "id_str": "17445752",
+        "id": 17445752,
+        "indices": [
+          60,
+          64
+        ],
+        "screen_name": "Cal"
+      },
+      {
+        "name": "Othman Laraki",
+        "id_str": "20495814",
+        "id": 20495814,
+        "indices": [
+          70,
+          77
+        ],
+        "screen_name": "othman"
+      }
+    ]
+  },
+  "text": "lecturing at the \"analyzing big data with twitter\" class at @cal with @othman  http://t.co/bfj7zkDJ",
+  "contributors": null,
+  "id": 240556426106372096,
+  "retweet_count": 3,
+  "in_reply_to_status_id_str": null,
+  "geo": {
+    "coordinates": [
+      37.871609,
+      -122.25831
+    ],
+    "type": "Point"
+  },
+  "retweeted": false,
+  "possibly_sensitive": false,
+  "in_reply_to_user_id": null,
+  "place": {
+    "name": "Berkeley",
+    "country_code": "US",
+    "country": "United States",
+    "attributes": {},
+    "url": "http://api.twitter.com/1/geo/id/5ef5b7f391e30aff.json",
+    "id": "5ef5b7f391e30aff",
+    "bounding_box": {
+      "coordinates": [
+        [
+          [
+            -122.367781,
+            37.835727
+          ],
+          [
+            -122.234185,
+            37.835727
+          ],
+          [
+            -122.234185,
+            37.905824
+          ],
+          [
+            -122.367781,
+            37.905824
+          ]
+        ]
+      ],
+      "type": "Polygon"
+    },
+    "full_name": "Berkeley, CA",
+    "place_type": "city"
+  },
+  "source": "Safari on iOS",
+  "user": {
+    "name": "Raffi Krikorian",
+    "profile_sidebar_fill_color": "DDEEF6",
+    "profile_background_tile": false,
+    "profile_sidebar_border_color": "C0DEED",
+    "profile_image_url": "http://a0.twimg.com/profile_images/1270234259/raffi-headshot-casual_normal.png",
+    "created_at": "Sun Aug 19 14:24:06 +0000 2007",
+    "location": "San Francisco, California",
+    "follow_request_sent": false,
+    "id_str": "8285392",
+    "is_translator": false,
+    "profile_link_color": "0084B4",
+    "entities": {
+      "url": {
+        "urls": [
+          {
+            "expanded_url": "http://about.me/raffi.krikorian",
+            "url": "http://t.co/eNmnM6q",
+            "indices": [
+              0,
+              19
+            ],
+            "display_url": "about.me/raffi.krikorian"
+          }
+        ]
+      },
+      "description": {
+        "urls": []
+      }
+    },
+    "default_profile": true,
+    "url": "http://t.co/eNmnM6q",
+    "contributors_enabled": false,
+    "favourites_count": 724,
+    "utc_offset": -28800,
+    "profile_image_url_https": "https://si0.twimg.com/profile_images/1270234259/raffi-headshot-casual_normal.png",
+    "id": 8285392,
+    "listed_count": 619,
+    "profile_use_background_image": true,
+    "profile_text_color": "333333",
+    "followers_count": 18752,
+    "lang": "en",
+    "protected": false,
+    "geo_enabled": true,
+    "notifications": false,
+    "description": "Director of @twittereng's Platform Services. I break things.",
+    "profile_background_color": "C0DEED",
+    "verified": false,
+    "time_zone": "Pacific Time (US & Canada)",
+    "profile_background_image_url_https": "https://si0.twimg.com/images/themes/theme1/bg.png",
+    "statuses_count": 5007,
+    "profile_background_image_url": "http://a0.twimg.com/images/themes/theme1/bg.png",
+    "default_profile_image": false,
+    "friends_count": 701,
+    "following": true,
+    "show_all_inline_media": true,
+    "screen_name": "raffi"
+  },
+  "in_reply_to_screen_name": null,
+  "in_reply_to_status_id": null
+};
