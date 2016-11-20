b62b49ad3861c102409185c0d0cbd0b19fabc5c4
wozaki <ozaki.shun5@gmail.com>
2016-11-20 10:27:20 +0900

    Navigate by swipe

--- redux/src/main/renderer/containers/MainContainerWrapper.js
+++ redux/src/main/renderer/containers/MainContainerWrapper.js
@@ -1,15 +1,26 @@
 import React, { Component, PropTypes } from 'react';
 import Header from '../components/Header';
 import Spinner from '../components/Spinner';
+import * as swipeNavigator from '../registries/swipeNavigator';
 
 export default function (InnerComponent) {
 
   class MainContainerWrapper extends Component {
+
+    componentDidMount() {
+      const { navigatableBySwipe } = this.props;
+      if (navigatableBySwipe) {
+        swipeNavigator.register(this.mainDOM)
+      }
+    }
+
     render() {
       const { title, isLoading } = this.props;
 
       return (
-        <div className="Main">
+        <div
+          className="Main"
+          ref={(node) => { this.mainDOM = node }}>
           <Header title={title}/>
           {isLoading ? <Spinner /> : null}
           <InnerComponent {...this.props} />
@@ -20,11 +31,13 @@ export default function (InnerComponent) {
 
   MainContainerWrapper.propTypes = {
     title: PropTypes.string.isRequired,
-    isLoading: PropTypes.bool
+    isLoading: PropTypes.bool,
+    navigatableBySwipe: PropTypes.bool
   };
 
   MainContainerWrapper.defaultProps = {
-    isLoading: false
+    isLoading: false,
+    navigatableBySwipe: false
   };
 
   return MainContainerWrapper;
