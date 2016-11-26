61ea9930faa72ba3874d7addbec4a6198d7d8cf6
wozaki <ozaki.shun5@gmail.com>
2016-11-26 15:18:19 +0900

    Remove the dependency on electron global variable

--- redux/src/main/renderer/new-tweet-window.js
+++ redux/src/main/renderer/new-tweet-window.js
@@ -3,9 +3,6 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import NewTweetContainer from './containers/NewTweetContainer.js'
 import configureStore from './store/configureStore'
-import * as registries from './registries/electron'
-
-window.registries = registries;
 
 render(
     <Provider store={configureStore()}>
