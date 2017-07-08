5421e3143c9e97f2994a74bd6dc6e9bd8ae79fcd
wozaki <ozaki.shun5@gmail.com>
2017-07-09 01:28:36 +0900

    Implement Media model

--- redux/src/main/domain/models/Media.js
+++ redux/src/main/domain/models/Media.js
@@ -1,7 +1,25 @@
+import fs from 'fs';
+
 export default class Media {
 
-    static get IMAGE_FILE_EXTENSIONS() {
-        return ['jpg', 'png', 'gif'];
-    }
+  static get IMAGE_FILE_EXTENSIONS() {
+    return ['jpg', 'jpeg', 'png', 'gif'];
+  }
+
+  static build(filePath) {
+    const buffer = fs.readFileSync(filePath);
+    const extension = filePath.split('.').pop();
+    // Media.IMAGE_FILE_EXTENSIONS.includes(extension)
+    return new Media(buffer, extension);
+  }
+
+  constructor(buffer, extension) {
+    this.buffer = buffer;
+    this.extension = extension;
+  }
+
+  toBase64() {
+    return this.buffer.toString('base64')
+  }
 
 }
